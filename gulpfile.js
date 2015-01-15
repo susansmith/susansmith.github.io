cbc1871fba65f3dec54e8bc33b656f7bfd86e7e2
Susan <susan.smith@udacity.com>
2015-01-15 02:08:33 -0500

    Added views directory to gulpfile minification and optimization

--- gulpfile.js
+++ gulpfile.js
@@ -15,32 +15,45 @@ gulp.task('images', function(cb) {
         optimizationLevel: 5,
         progressive: true,
         interlaced: true
-    })).pipe(gulp.dest('img')).on('end', cb).on('error', cb);
+    })).pipe(gulp.dest('./img')).on('end', cb).on('error', cb);
+    gulp.src(['../src/views/images/*.png','../src/views/images/*.jpg','../src/views/images/*.gif','../src/views/images/*.jpeg']).pipe(imageop({
+        optimizationLevel: 5,
+        progressive: true,
+        interlaced: true
+    })).pipe(gulp.dest('./views/images')).on('end', cb).on('error', cb);
 });
 
 // Minify CSS
 gulp.task('minify-css', function() {
   gulp.src('../src/css/*.css')
     .pipe(minifyCSS({keepBreaks:true}))
-    .pipe(gulp.dest('./dist/css'))
+    .pipe(gulp.dest('./css'));
+  gulp.src('../src/views/css/*.css')
+    .pipe(minifyCSS({keepBreaks:true}))
+    .pipe(gulp.dest('./views/css'))
 });
 
 
 // Minify HTML
 gulp.task('minify-html', function() {
     var opts = {comments:true,spare:true};
-
      gulp.src('../src/*.html')
-    .pipe(minifyHTML(opts))
-    .pipe(gulp.dest('./'))
+      .pipe(minifyHTML(opts))
+      .pipe(gulp.dest('./'));
+    gulp.src('../src/views/*.html')
+      .pipe(minifyHTML(opts))
+      .pipe(gulp.dest('./'))
 });
 
 
-// Compress JS
+// Compress JS at root
 gulp.task('compress', function() {
-  gulp.src('../src/lib/*.js')
+  gulp.src('../src/js/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest('./'));
+  gulp.src('../src/views/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./views/js'))
 });
 
 
